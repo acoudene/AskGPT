0f7fd4c648316a31e16ecb63ffc10d00cae33ecf
Anthony COUDENE <anthony.coudene@technidata-web.com>
2023-01-29 19:31:05 +0100

    Add default parameters

--- AskGPT/OpenAIConfiguration.cs
+++ AskGPT/OpenAIConfiguration.cs
@@ -10,23 +10,23 @@ namespace AskGPT
   {
     public const string OpenAIConfigurationKey = "OPENAI";
 
-    public string Key { get; set; } = string.Empty;
+    public string Key { get; set; } = "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";
 
-    public string Url { get; set; } = string.Empty;
+    public string Url { get; set; } = "https://api.openai.com/v1/completions";
 
-    public string Model { get; set; } = string.Empty;
+    public string Model { get; set; } = "text-davinci-003";
 
-    public int Temperature { get; set; }
+    public int Temperature { get; set; } = 0;
 
-    public int MaxTokens { get; set; }
+    public int MaxTokens { get; set; } = 100;
 
-    public float TopP { get; set; }
+    public float TopP { get; set; } = 1.0f;
 
-    public float FrequencyPenalty { get; set; }
+    public float FrequencyPenalty { get; set; } = 0.2f;
 
-    public float PresencePenalty { get; set; }
+    public float PresencePenalty { get; set; } = 0.0f;
 
-    public string[] Stop { get; set; } = Array.Empty<string>();
+    public string[] Stop { get; set; } = { "\n" };
 
   }
 }
